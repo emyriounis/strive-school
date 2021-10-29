7572111e0981557d781f3585e84a22846ac4dba5
hilina88 <hilinawuhib@gmail.com>
2021-10-29 15:54:11 +0200

    Update index.js

--- Homework/M3-D5/artist-page/index.js
+++ Homework/M3-D5/artist-page/index.js
@@ -179,7 +179,7 @@ fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem")
       .map((song) => {
         return `
         <tr class="tracklist-row"> 
-        <th id="green-row" scope="row" colspan="4" class="track-title-text">
+        <th  scope="row" colspan="4" class="track-title-text">
             <svg  class="playing-progres-icon" xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-bar-chart-line" viewBox="0 0 16 16">
             <path d="M11 2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h1V7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 
             1v7h1V2zm1 12h2V2h-2v12zm-3 0V7H7v7h2zm-5 0v-3H2v3h2z"/>
